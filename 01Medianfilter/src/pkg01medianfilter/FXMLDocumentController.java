7d7683f473a3e3e96f55950e993145d7cb4deff3
Joel Strasser <strasser999@gmail.com>
2019-10-24 20:04:00 +0200

    added apache commons math3 for median calcuclation;

--- 01Medianfilter/src/pkg01medianfilter/FXMLDocumentController.java
+++ 01Medianfilter/src/pkg01medianfilter/FXMLDocumentController.java
@@ -11,6 +11,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.PixelReader;
@@ -19,6 +20,7 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
+import org.apache.commons.math3.stat.descriptive.rank.Median;
 
 public class FXMLDocumentController {
 
@@ -60,6 +62,9 @@ public class FXMLDocumentController {
 
     @FXML
     private Text text_status;
+    
+    @FXML
+    private TextField median_filter;
 
     @FXML
     void onAction(ActionEvent event) {
@@ -221,7 +226,95 @@ public class FXMLDocumentController {
         }
         
         if(event.getSource().equals(menuitem_applymedian)) {
-            
+            CompletableFuture
+                .supplyAsync(() -> {
+                    final Text status = text_status;
+                    final ProgressBar pB = progressbar_currentlydoing;
+                    final int height=(int)imageview_current.getImage().getHeight();
+                    final int width=(int)imageview_current.getImage().getWidth();
+                    PixelReader pixelReader=imageview_current.getImage().getPixelReader();
+                    WritableImage writableImage = new WritableImage(width,height);
+                    PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+                    long cTime = System.currentTimeMillis();
+                    
+                    int r = Integer.parseInt(median_filter.getText());
+                    int runex = 0 + 1 + 2 * r;
+                    
+                    
+                    for (int y = 0; y < height; y++){
+                        for (int x = 0; x < width; x++){
+                            double bv[] = new double[runex * runex];
+                            double rv[] = new double[runex * runex];
+                            double gv[] = new double[runex * runex];
+                            int addCount = 0;
+                            for(int a = x - r; a  < x + 1 + r; a++) {
+                                for(int b = y - r; b  < y + 1 * r; b++) {
+                                    
+                                    if(a < 0 || b < 0) continue;
+                                    
+                                    Color color;
+                                    try {
+                                        color = pixelReader.getColor(a, b);
+                                    } catch (Exception e) {
+                                        continue;
+                                    }
+                                    
+                                    rv[addCount] = color.getRed();
+                                    gv[addCount] = color.getGreen();
+                                    bv[addCount] = color.getBlue();
+                                    
+                                    addCount++;
+                                }
+                            }
+                            
+                            Median median = new Median();
+                            
+                            double rm = median.evaluate(rv, 0, addCount);
+                            double bm = median.evaluate(bv, 0, addCount);
+                            double gm = median.evaluate(gv, 0, addCount);
+                            
+                            if(System.currentTimeMillis() - cTime > 1000) {
+                                cTime = System.currentTimeMillis();
+                                final int xs = x;
+                                final int ys = y;
+                                Platform.runLater(() -> {
+                                    status.setText("Filtering red on image @ X:" + xs + ", Y:" + ys);
+                                    pB.setProgress((xs+1*ys+1)/(height*width));
+                                });
+                            }
+                            
+                            for(int a = x - r; a  < x + 1 +  r; a++) {
+                                for(int b = y - r; b  < y + 1 + r; b++) {
+                                    
+                                    if(a < 0 || b < 0) continue;
+                                    
+                                    try {
+                                        Color color2 = pixelReader.getColor(a, b);
+                                        Color c = new Color(rm, gm, bm, color2.getOpacity());
+                                        pixelWriter.setColor(a, b, c);
+                                    } catch (Exception e) {
+                                        
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+
+                    return writableImage;                    
+                }).whenComplete((result, error) -> {
+                    Platform.runLater(() -> {
+                        if(error != null) {
+                            text_status.setText("Error");
+                            progressbar_currentlydoing.setProgress(0);
+                            return;
+                        }
+                        text_status.setText("Done");
+                        progressbar_currentlydoing.setProgress(1);
+                        imageview_modified.setImage(result);
+                    });
+                });
         }
         
         if(event.getSource().equals(menuitem_about)) {
